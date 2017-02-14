b17a2b65087d81bd49c6a97e09ea4bde18ce3243
hzxiongxu <xx@xiongxudeMacBook-Pro.local>
2017-02-14 20:29:54 +0800

    配置修改

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 var path = require('path');
 var webpack = require("webpack");
-var precss = require("precss")
-var autoprefixer = require("autoprefixer")
+var htmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-console.log(path.resolve(__dirname,'./src/output/'));
 module.exports = {
     entry:"./src/js/index.js",
     output:{
         path:path.resolve(__dirname,'./src/output/'),
-        filename:'wue.js'
+        filename:'[name]-[hash].js'
     },
     module:{
         loaders:[
@@ -34,9 +33,33 @@ module.exports = {
             }
         ]
     },
-   /* resolve: {
-        root: path.resolve('./')
-    },*/
+    resolve: {
+        extensions: ['.js','.vue'],
+        //root:["../node_modules"],
+        alias:{
+            "vue":"./src/js/lib/vue.js"
+        }
+    },
+    plugins:[
+        new webpack.BannerPlugin("create by hzxiongxu"),
+        new htmlWebpackPlugin({
+            template:__dirname+"/src/template/index.html"
+        }),
+        new ExtractTextPlugin("[name]-[hash].css")
+    ],
+    /*postcss:[
+        require("autoprefixer")
+    ],*/
+    devServer:{
+        //contentVase: ".src/template",
+        hot:true,
+        historyApiFallback:true,
+        inline:true,
+        process:true,
+    }
+    /*scripts:{
+      "start":"webpack-dev-server --hot --inline"
+    }*/
    /* plugins:{
         new webpack.LoaderOptionsplugin({
             options: {
